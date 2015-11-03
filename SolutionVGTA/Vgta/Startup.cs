4fbad5ff192690158d512f08eaa9bcbf5865322d
aminehaddar <amine.haddar@esprit.tn>
2015-11-03 16:06:31 +0100

    initial commit

---
+++ SolutionVGTA/Vgta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vgta.Startup))]
+namespace Vgta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
